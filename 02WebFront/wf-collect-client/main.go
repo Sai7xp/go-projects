8cbabd01e09a8e6e7af097135f2a920e379c2433
Sai7xp <saisumanth2303@gmail.com>
2024-03-09 00:19:25 +0530

    Kafka Producer implementation - triggers new message when /collect api receives new build request

--- 02WebFront/wf-collect-client/main.go
+++ 02WebFront/wf-collect-client/main.go
@@ -10,37 +10,44 @@ import (
 	"collectclient/utils"
 	"encoding/json"
 	"fmt"
-	"github.com/go-playground/validator/v10"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/IBM/sarama"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
 )
 
-var logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+var logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime) // logger
+var validate *validator.Validate                               // validator
 
-// validator
-var validate *validator.Validate
+const (
+	kafkaTopic = "webfront-kafka"
+)
 
-// entry point
 func main() {
 	fmt.Println("wf-collect-client Microservice Started")
-
-	router := mux.NewRouter()
 	validate = validator.New(validator.WithRequiredStructEnabled())
 
+	/// Create New Route
+	router := mux.NewRouter().PathPrefix("/api/v1").Subrouter()
+
 	/// POST api for collecting details of build
-	router.HandleFunc("/api/v1/collect", collectDetailsHandler).Methods("POST")
+	router.HandleFunc("/collect", collectDetailsHandler).Methods("POST")
+	router.HandleFunc("/build/{build_id}", showBuildEventsHandler).Methods("GET")
+
 	/// wrong route handler
 	router.NotFoundHandler = http.HandlerFunc(routeNotFoundHandler)
-
 	fmt.Println("Server started at port 3000")
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
 
-// 🛠️ Handler for /api/v1/collect API (allowed methods POST)
+// /
+// 🛠️ Handler for /api/v1/collect API (only POST is allowed)
+// /
 func collectDetailsHandler(w http.ResponseWriter, req *http.Request) {
-	var projecDetails models.WebfrontCollectDetails
+	projecDetails := new(models.WebfrontCollectDetails)
 	/// decode data
 	if err := json.NewDecoder(req.Body).Decode(&projecDetails); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -51,17 +58,74 @@ func collectDetailsHandler(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	/// generate random build id
 	buildId := utils.GenerateBuildId()
 
-	var response = make(map[string]interface{})
-	response["success"] = true
-	response["buildId"] = buildId
+	// send details along with buildId to another microservice `wf-code-builder` via kafka
+	var builderReqBody = map[string]interface{}{
+		"buildId":            buildId,
+		"project_github_url": projecDetails.ProjectGithubUrl,
+		"build_command":      projecDetails.BuildCommand,
+		"build_out_dir":      projecDetails.BuildOutDir,
+	}
+	buildDetailsBytes, _ := json.Marshal(builderReqBody)
+
+	// 🗣️ add new message to kafka
+	kafkaErr := pushBuildDetailsToKafka(buildDetailsBytes)
+	if kafkaErr != nil {
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"success": false,
+			"message": kafkaErr,
+			"buildId": nil,
+		})
+		return
+	}
+
+	/// return success message to user along with the build id
 	logger.Printf("New Build request!! Assigned buildId %s", buildId)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"success": true,
+		"message": "New build request added to the queue successfully!✅",
+		"buildId": buildId,
+	})
+}
 
-	// TODO: send details along with buildId to another microservice `wf-code-builder`
-	// sendDetailsToCodeBuilder()
+func createKafkaProducer(url []string) (sarama.SyncProducer, error) {
+	kafkaConfig := sarama.NewConfig()
+	kafkaConfig.Producer.Return.Successes = true
+	kafkaConfig.Producer.RequiredAcks = sarama.WaitForAll
+	kafkaConfig.Producer.Retry.Max = 5
+
+	producer, err := sarama.NewSyncProducer(url, kafkaConfig)
+	if err != nil {
+		return nil, err
+	}
+	/// return producer and no error
+	return producer, nil
+}
+
+// sends new message to kafka
+func pushBuildDetailsToKafka(buildDetailsBytes []byte) error {
+
+	url := []string{"localhost:29092"}
+	producer, err := createKafkaProducer(url)
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
+
+	message := &sarama.ProducerMessage{
+		Topic: kafkaTopic,
+		Value: sarama.ByteEncoder(buildDetailsBytes),
+	}
+	_, _, err = producer.SendMessage(message)
+	if err != nil {
+		return err
+	}
+	log.Printf("Produced new message in topic %s", kafkaTopic)
+	return nil
+
 }
 
 // 404 route not found handler
@@ -69,4 +133,9 @@ func routeNotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	logger.Println("Route Not Found")
 	w.WriteHeader(http.StatusNotFound)
 	w.Write([]byte(`{"message":"OOPS! Wrong Route"}`))
+}
+
+// Get Build events handler
+func showBuildEventsHandler(w http.ResponseWriter, r *http.Request) {
+
 }
